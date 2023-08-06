bc6974e58a90607067b6f78e2c203323502e173a
cayean <collinayala@gmail.com>
2023-08-05 21:50:54 -0500

    added products route, pug file, and added route to app.js

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+// routes initization
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const aboutRouter = require("./routes/about");
+const productsRoutes = require("./routes/products");
 
 const app = express();
 
@@ -20,9 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// PUG routes
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/about", aboutRouter);
+app.use("/products", productsRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/products.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// GET products lists
+router.get("/", function (req, res, next) {
+  res.render("products", { title: "FTG products" });
+});
+
+module.exports = router;
