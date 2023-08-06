a346b5bfdfa269cddc2bffdc7e509c0fd6a9c749
cayean <collinayala@gmail.com>
2023-08-05 20:27:14 -0500

    refactored routes/about.js. route now works appropriately

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // GET about page for fresh table greens
-router.get("/about", function (req, res, next) {
+router.get("/", function (req, res, next) {
   res.render("about", { title: "About Fresh Table Greens" });
 });
 
