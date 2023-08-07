f543d84020c9f8c121c41ec03163dbe3261c63c6
cayean <collinayala@gmail.com>
2023-08-07 08:51:49 -0500

    product route added. 404 status error resolved

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // GET products lists
 router.get("/", function (req, res, next) {
-  res.render("products", { title: "FTG products" });
+  res.render("products", { title: "FTG products & services" });
 });
 
 module.exports = router;
