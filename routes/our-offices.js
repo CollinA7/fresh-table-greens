1217f3de9c65df9419a421073ec7dc54e3aba36d
cayean <collinayala@gmail.com>
2023-08-07 09:09:17 -0500

    initial routes completed

--- routes/our-offices.js
+++ routes/our-offices.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // GET our offices
 router.get("/", function (req, res, next) {
-  res.render("our-offices", { title: "offices of Fresh Table" });
+  res.render("our-offices", { title: "Offices of Fresh Table" });
 });
 
 module.exports = router;
